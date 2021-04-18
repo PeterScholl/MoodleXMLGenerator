f9f918dee42e74ecd91bbe2695b0dfb00c4cd17b
Peter Scholl <scholl@unterrichtsportal.org>
2021-04-18 11:30:26 +0200

    Bereinigung zum Test

--- src/aufgabenGenerator/ManageXML.java
+++ src/aufgabenGenerator/ManageXML.java
@@ -6,10 +6,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.w3c.dom.Node;
-import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 
-import java.io.File;
 import java.io.StringReader;
 
 public class ManageXML {
